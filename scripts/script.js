dedc08a4c6827a0b8f8cd902031dce0dce5497b2
ericcnguyen04 <ericc.win@gmail.com>
2022-12-01 22:32:29 -0500

    ground added

--- scripts/script.js
+++ scripts/script.js
@@ -3,8 +3,8 @@ const canvas = document.getElementById('game-screen')
 // set thecanvas resolution to be the same as the screen
 canvas.setAttribute('height', getComputedStyle(canvas)['height'])
 canvas.setAttribute('width',getComputedStyle(canvas)['width'])
-//console.log(canvas.height) //439
-//console.log(canvas.width) //879
+console.log(canvas.height) //648
+console.log(canvas.width) //1094
 
 
 //get rendering context from canvas
@@ -28,30 +28,32 @@ class Bird {
     }
 }
 
-class Pipe {
-    constructor(x, y, width, h1, h2) {
-        this.x = x
-        this.y = y
-        this.width = width
-        // this.gap = gap
-        this.h1 = h1
-        this.h2 = h2
-        // this.h3 = h3
-        // this.h4 = h4
-    }
+// class Pipe {
+//     constructor(x, y, width, h1, h2) {
+//         this.x = x
+//         this.y = y
+//         this.width = width
+//         // this.gap = gap
+//         this.h1 = h1
+//         this.h2 = h2
+//         // this.h3 = h3
+//         // this.h4 = h4
+//     }
 
-    render() {
-        ctx.fillStyle = this.color
-        ctx.fill(this.x, this.y, this.width, this.h1)
-        ctx.fill(this.x, this.y, this.width, this.h2)
-    }
-}
+//     render() {
+//         ctx.fillStyle = this.color
+//         ctx.fill(this.x, this.y, this.width, this.h1)
+//         ctx.fill(this.x, this.y, this.width, this.h2)
+//     }
+// }
 
 //testing
 const testBird = new Bird(250, 270, 50, 50, 'red')
 testBird.render()
-const testPipe = new Pipe(700, 0, 75, 200, 300)
-testPipe.render()
+const ground = new Bird(0, 500, canvas.width, 100, 'brown')
+ground.render()
+// const testPipe = new Pipe(700, 0, 75, 200, 300)
+// testPipe.render()
 
 // ========== //
 
@@ -65,13 +67,13 @@ function controlInput() {
     }
 }
 
+const pressedKeys = {}
 document.addEventListener('keydown', e => pressedKeys[e.key] = true)
 // document.addEventListener('keyup', e => pressedKeys[e.key] = false)
 controlInput()
 // ========== //
 
 // === ! Step 3: DEFINE GAME LOOP ! === //
-const pressedKeys = {}
 
 function gameLoop() {
     //clearing canvas
